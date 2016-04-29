aab99ccf65a92a20f220201fa34f31716a31566e
Daniel Reinke <djreinke@olivet.edu>
2016-04-29 16:40:14 -0500

    Adds PHP homepage and includes

---
+++ includes/navigation.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img alt="Wunderful Tails Mobile Pet Salon Morris Country NJ" src="assets/images/wunderful-tails-logo.jpg"></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Mobile Pet Salon</a></li>
+            <li><a href="#about">Services</a></li>
+            <li><a href="#contact">About Us</a></li>
+            <li><a href="#contact">Make an Appointment</a></li>
+          </ul>
+          <div class="navbar-social-wrap">
+          <a href="https://www.facebook.com/WunderfulTails/" target="_blank"><div class="navbar-social facebook"></div></a>
+          </div>
+        </div><!--/.nav-collapse -->
+      </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="Wunderful Tails">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Wunderful Tails Mobile Pet Salon</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/webfont/font.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+
+  <div class="top-bar">
+    <div class="container">
+      Mobile Pet Salon in Morris County, New Jersey - Phone: (973)-207-3946 Email: info@wunderfultails.com
+    </div>
+  </div>
+  <?php include 'includes/navigation.php' ?>
+
+    <div class="jumbotron homepage-feat">
+      <div class="container">
+        <div class="col-md-9 col-md-offset-3 homepage-feat-meta">
+          <h1 class="center">Full Service Dog Grooming at Your Doorstep</h1>
+          <div class="homepage-feat-truck"></div>
+          <p><button type="button" class="btn btn-primary btn-lg float-center">Make an Appointment</button></p>
+        </div>
+        <div class="homepage-feat-duke"></div>
+      </div>
+    </div>
+    <div class="sec">
+      <div class="container">
+      <div class="row">
+        <div class="col-sm-4 center"><h3>Mobile Dog Grooming</h3><img class="img-full" src="assets/images/home_feat01.jpg"></div>
+        <div class="col-sm-4 center"><h3>Deshedding Services</h3><img class="img-full" src="assets/images/home_feat02.jpg"></div>
+        <div class="col-sm-4 center"><h3>Skincare Treatments</h3><img class="img-full" src="assets/images/home_feat03.jpg"></div>
+      </div>
+        <p><button type="button" class="btn btn-primary btn-lg float-center buf-md">See All Services</button></p>
+      </div><!-- /.container -->
+    </div>
+    <div class="sec grey">
+      <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <img class="img-full" src="assets/images/hydro-massage.jpg">
+        </div>
+        <div class="col-sm-7 col-sm-offset-1" id="hydro-massage">
+          <h3>Hydro Massage Therapy Bath</h3>
+          <p>Hydro-massage bathing is an innovative development in bathing for dogs. This unique system produces a gentle, combing spray of shampoo and water using a hand-held massage sprayer. The shampoo solution penetrates your pet's fur and gets to the skin to remove dirt, debris, loose fur, dead skin, and dander, resulting in the healthiest skin possible. Your pets are sure to love the massage-like treatment.</p>
+          <p><button type="button" class="btn btn-primary btn-lg buf-sm">Read More</button></p>
+        </div>
+      </div>
+      </div>
+    </div>
+    <div class="sec">
+      <div class="container">
+        <img class="center float-center" src="assets/images/ico-van.png">
+        <h3 class="center">Why Mobile Dog Grooming?</h3>
+        <p class="lead center buf-lg-bottom">Some text here to explain the overview of benefits</p>
+        <div class="row">
+        <div class="col-sm-6"><h4><strong>Benefits to your Pet</strong></h4>
+        <ul>
+          <li>Less stress on the dog</li>
+          <li>No strange dogs interacting with your dog while in our van. Unlike a shop.</li>
+          <li>One on one service.. unlike in a shop who has multiple people.</li>
+          <li>Still can see they are home while being groomed.</li>
+          <li>No CAGE dryers or crates to sit in while grooming. Unlike shops use drying cages.</li>
+          <li>No multiple trips in the car to and from a shop.</li>
+        </ul>
+        </div>
+        <div class="col-sm-6"><h4><strong>Benefits to your Family</strong></h4>
+        <ul>
+          <li>Less stress on your family</li>
+          <li>No car rides with a dirty dog in the clean car</li>
+          <li>No worries what is going on with our pet while in the shop.</li>
+          <li>Ready in about an hour for most breeds.</li>
+        </ul>
+        </div>
+      </div>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="container">
+      <div class="row">
+        <div class="col-sm-6">© Wunderful Tails Mobile Pet Salon LLC. All Rights Reserved. 2016.</div>
+        <div class="col-sm-6">
+          <nav class="footer-nav">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Mobile Pet Salon</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Make an Appointment</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">Randolph | Morristown | Dover | Rockaway | Denville | Morris Plains</div>
+        <div class="col-sm-6"></div>
+      </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
