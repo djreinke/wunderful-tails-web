bb4facc50a8a20632a8aff7ae3ff7e496420633f
Daniel Reinke <djreinke@olivet.edu>
2016-04-29 16:43:14 -0500

    Adds footer.php

---
+++ includes/footer.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+      <div class="container">
+      <div class="row">
+        <div class="col-sm-6">© Wunderful Tails Mobile Pet Salon LLC. All Rights Reserved. 2016.</div>
+        <div class="col-sm-6">
+          <nav class="footer-nav">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Mobile Pet Salon</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Make an Appointment</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6">Randolph | Morristown | Dover | Rockaway | Denville | Morris Plains</div>
+        <div class="col-sm-6"></div>
+      </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
